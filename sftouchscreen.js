24437c7df818c7cb5a1219a868360082a1c6a0d2
mehrpadin <mail@mehrpadin.net>
2012-02-19 15:49:48 +0330

    Version 1.0

---
+++ sftouchscreen.js
@@ -0,0 +1,40 @@
+/*
+ * sf-Touchscreen v1.0b - Provides touchscreen compatibility for the jQuery Superfish plugin.
+ *
+ * Developer's note:
+ * Built as a part of the Superfish project for Drupal (http://drupal.org/project/superfish) 
+ * Found any bug? have any cool ideas? contact me right away! http://drupal.org/user/619294/contact
+ *
+ * jQuery version: 1.3.x or higher.
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ *  http://www.opensource.org/licenses/mit-license.php
+ *  http://www.gnu.org/licenses/gpl.html
+ */
+
+(function($){
+  $.fn.sftouchscreen = function() {
+    // Return original object to support chaining.
+    return this.each( function() {
+      // Select hyperlinks from parent menu items.
+      $(this).find('li > ul').closest('li').children('a').each( function() {
+        var $item = $(this);
+        // No .toggle() here as it's not possible to reset it.
+        $item.click( function(event){
+          // Already clicked? proceed to the URI.
+          if ($item.hasClass('sf-clicked')) {
+            var $uri = $item.attr('href');
+            window.location = $uri;
+          }
+          else {
+            event.preventDefault();
+            $item.addClass('sf-clicked');
+          }
+        }).closest('li').mouseleave( function(){
+          // So, we reset everything.
+          $item.removeClass('sf-clicked');
+        });
+      });
+    });
+  };
+})(jQuery);
